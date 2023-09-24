3165f652bb8bba4ca0fd631612c211b883ef9e21
Olufemi Ojeyemi <olufemiojeyemi@gmail.com>
2023-09-24 18:09:09 +0100

    Stopped at 31 minutes

--- episode1/src/App.jsx
+++ episode1/src/App.jsx
@@ -1,7 +1,25 @@
 import './App.css';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <div>
+                <User />
+                <User />
+                <User />
+            </div>
+        </div>
+    );
+}
+
+function User() {
+    return (
+        <div>
+            <h1>Femi</h1>
+            <h2>23</h2>
+            <h2>olufemiojeyemi@gmail.com</h2>
+        </div>
+    );
 }
 
 export default App;
