d4b8b39c43dde68d27ab5bc5d694859913504abc
Femosky <olufemiojeyemi@gmail.com>
2023-09-25 15:50:46 +0100

    Stopped at 1:02:57 minutes

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
+import { User } from './Users';
+import { planets } from './Planets';
+
+const age = 15;
+
+age >= 18 ? console.log('A') : console.log('B');
 
 function App() {
+    // const users = [
+    //     { name: 'Pedro', age: 21 },
+    //     { name: 'Jake', age: 25 },
+    //     { name: 'Jessica', age: 45 },
+    // ];
+    // return (
+    //     <div className="App">
+    //         {users.map((user, key) => {
+    //             return <User name={user.name} age={user.age} />;
+    //         })}
+    //     </div>
+    // );
+
     return (
         <div className="App">
-            <h1 className="name">Femi</h1>
+            {planets.map((planet) => {
+                return <div>{planet.isGasPlanet ? planet.name : ''}</div>;
+            })}
         </div>
     );
 }
 
 export default App;
+
+// function App() {
+//     return <div className='App'></div>;
+// }
